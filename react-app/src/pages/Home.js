0cb91b24b05238d87fbf22859c96a5300a4cf025
Supatat Singkomfu <Supatat Singkomfu>
2020-12-10 11:52:32 +0700

    Edit Home

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -3,32 +3,23 @@ import styled from 'styled-components'
 import List from '../components/List'
 import Graph from '../components/Graph'
 import Ranking from '../components/Ranking'
+import Banner from '../assets/banner.png'
 import {useDispatch} from 'react-redux'
 import store from '../redux/store'
 import {currently} from '../redux/actions/Temporature'
 
 
 const Title = styled.div`
-    display: flex;
-    align-items: center;
-    height: 90vh;
-    background-color: #CFE7F7;
-    color: #fff;
-    h1{
-        font-size: 100px;
-        font-weight: bold;
-        margin-left: 15%;
+    img{
+        width: 100%;
     }
 `
 
-
-
 const Home = () => {
-    
     return (
         <>
             <Title>
-                <h1>ATEMP</h1>
+            <img src={Banner} />
             </Title>
             <List />
             <Graph />

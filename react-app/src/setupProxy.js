fad891f7f85128a2371892b9653452e4ca1bf70e
Nat <nat.wrw@gmail.com>
2020-12-11 12:20:13 +0700

    Add proxy to database

--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = function (app) {
   app.use(
-    '/api',
+    '/query',
     createProxyMiddleware({
-    //   target: 'http://database.trwl.club:8086',
-        target: 'http://localhost:4000',
+      //   target: 'http://database.trwl.club:8086',
+      target: 'http://localhost:4000',
 
       changeOrigin: false,
     })

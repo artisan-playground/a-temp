1ed63d38a08001364349a11d5f0e98b4703d9232
Supatat Singkomfu <Supatat Singkomfu>
2020-12-13 15:06:54 +0700

    Edit List in components

--- react-app/src/components/List/index.js
+++ react-app/src/components/List/index.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-import React,{useState,useContext} from 'react'
-import { ListTemp, ListComponents, CompoLeft, CompoRight, RealTime, BoxRealTime, RTDetails, Top, Bottom } from './ListElements'
-import {TempValueContextWrapper} from '../../contexts/TempValueContext'
-=======
-import React, { useState } from 'react'
->>>>>>> 577cd62abff980cde23b738dee2f7498330bf416
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { TempValueContextWrapper } from '../../contexts/TempValueContext'
 import {
   Bottom,
   BoxRealTime,
@@ -26,94 +21,8 @@ const Value = styled.div`
   }
 `
 
-<<<<<<< HEAD
 const List = () => {
-    const {temp1,maxTemp,minTemp,tempAvg}= useContext(TempValueContextWrapper)
-
-    // fetch(
-    //     '/query?db=dii&q=SELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FArm-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)%3BSELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FBoat-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)&epoch=ms'
-    // )
-
-    return (
-        <RealTime>
-            <ListTemp>
-                <ListComponents>
-                    <CompoLeft>
-                        <h1>Max</h1>
-                    </CompoLeft>
-                    <CompoRight>
-                        <Value>
-                            {maxTemp}                            
-                        </Value>
-                    </CompoRight>
-                </ListComponents>
-                <ListComponents>
-                    <CompoLeft>
-                        <h1>Min</h1>
-                    </CompoLeft>
-                    <CompoRight>
-                        <Value>
-                            {minTemp}                   
-                        </Value>
-                    </CompoRight>
-                </ListComponents>
-                <ListComponents>
-                    <CompoLeft>
-                        <h1>Average</h1>
-                    </CompoLeft>
-                    <CompoRight>
-                        <Value>
-                            {tempAvg}
-                        </Value>
-                    </CompoRight>
-                </ListComponents>
-            </ListTemp>
-            <BoxRealTime>
-                <RTDetails>
-                    <Top>
-                        <h2>NOW</h2>
-                    </Top>
-                    <Bottom>
-                        <div>
-                            {temp1}
-                        </div>
-                    </Bottom>
-                </RTDetails>
-            </BoxRealTime>
-        </RealTime>
-    )
-=======
-const mqtt = require('mqtt')
-const options = {
-  protocol: 'mqtts',
-  clientId: 'b0908853' + Math.random(),
-}
-const client = mqtt.connect('mqtt://mqtt.artisandigital.tech:8883', options)
-client.subscribe('dii/Boat-001/status')
-
-let note
-let minTemp = 0
-let maxTemp = 0
-let min = 0
-
-const List = () => {
-  const [temp, setTemp] = useState()
-
-  client.on('connect', function () {
-    console.log('connect')
-  })
-  client.on('message', function (topic, message) {
-    note = message.toString()
-    note = JSON.parse(note)
-    if (note.d.myName === 'Boat-001') {
-      setTemp(note.d.temperature)
-    }
-  })
-
-  if (temp > maxTemp) {
-    maxTemp = temp
-  }
-  //   http://localhost:4000/query?db=dii&q=SELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FArm-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)%3BSELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FBoat-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)&epoch=ms
+  const { temp1, maxTemp, minTemp, tempAvg } = useContext(TempValueContextWrapper)
 
   // fetch(
   //     '/query?db=dii&q=SELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FArm-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)%3BSELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FBoat-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)&epoch=ms'
@@ -142,7 +51,9 @@ const List = () => {
           <CompoLeft>
             <h1>Average</h1>
           </CompoLeft>
-          <CompoRight>{/* <DisplayAvgTemporature /> */}</CompoRight>
+          <CompoRight>
+            <Value>{tempAvg}</Value>
+          </CompoRight>
         </ListComponents>
       </ListTemp>
       <BoxRealTime>
@@ -151,13 +62,12 @@ const List = () => {
             <h2>NOW</h2>
           </Top>
           <Bottom>
-            <div>{temp}</div>
+            <div>{temp1}</div>
           </Bottom>
         </RTDetails>
       </BoxRealTime>
     </RealTime>
   )
->>>>>>> 577cd62abff980cde23b738dee2f7498330bf416
 }
 
 export default List

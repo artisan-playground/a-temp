f3793dd309437113e0aa38736d1423c3c0dbf51f
charnchon <boatcharnchon@gmail.com>
2020-12-13 15:31:07 +0700

    UPDATE ranking

--- react-app/src/components/List/index.js
+++ react-app/src/components/List/index.js
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-import React,{useState,useContext} from 'react'
-import { ListTemp, ListComponents, CompoLeft, CompoRight, RealTime, BoxRealTime, RTDetails, Top, Bottom } from './ListElements'
-import {TempValueContextWrapper} from '../../contexts/TempValueContext'
-=======
-import React, { useState } from 'react'
->>>>>>> 577cd62abff980cde23b738dee2f7498330bf416
+import React, { useContext } from 'react'
 import styled from 'styled-components'
-import {
-  Bottom,
-  BoxRealTime,
-  CompoLeft,
-  CompoRight,
-  ListComponents,
-  ListTemp,
-  RealTime,
-  RTDetails,
-  Top,
-} from './ListElements'
+import { TempValueContextWrapper } from '../../contexts/TempValueContext'
+import { Bottom, BoxRealTime, CompoLeft, CompoRight, ListComponents, ListTemp, RealTime, RTDetails, Top } from './ListElements'
 
 const Value = styled.div`
   font-size: 25px;
@@ -26,7 +11,6 @@ const Value = styled.div`
   }
 `
 
-<<<<<<< HEAD
 const List = () => {
     const {temp1,maxTemp,minTemp,tempAvg}= useContext(TempValueContextWrapper)
 
@@ -43,7 +27,7 @@ const List = () => {
                     </CompoLeft>
                     <CompoRight>
                         <Value>
-                            {maxTemp}                            
+                            {maxTemp}
                         </Value>
                     </CompoRight>
                 </ListComponents>
@@ -53,7 +37,7 @@ const List = () => {
                     </CompoLeft>
                     <CompoRight>
                         <Value>
-                            {minTemp}                   
+                            {minTemp}
                         </Value>
                     </CompoRight>
                 </ListComponents>
@@ -82,82 +66,6 @@ const List = () => {
             </BoxRealTime>
         </RealTime>
     )
-=======
-const mqtt = require('mqtt')
-const options = {
-  protocol: 'mqtts',
-  clientId: 'b0908853' + Math.random(),
-}
-const client = mqtt.connect('mqtt://mqtt.artisandigital.tech:8883', options)
-client.subscribe('dii/Boat-001/status')
-
-let note
-let minTemp = 0
-let maxTemp = 0
-let min = 0
-
-const List = () => {
-  const [temp, setTemp] = useState()
-
-  client.on('connect', function () {
-    console.log('connect')
-  })
-  client.on('message', function (topic, message) {
-    note = message.toString()
-    note = JSON.parse(note)
-    if (note.d.myName === 'Boat-001') {
-      setTemp(note.d.temperature)
-    }
-  })
-
-  if (temp > maxTemp) {
-    maxTemp = temp
-  }
-  //   http://localhost:4000/query?db=dii&q=SELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FArm-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)%3BSELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FBoat-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)&epoch=ms
-
-  // fetch(
-  //     '/query?db=dii&q=SELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FArm-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)%3BSELECT%20mean(%22d_temperature%22)%20FROM%20%22v1%22%20WHERE%20(%22topic%22%20%3D%20%27dii%2FBoat-001%2Fstatus%27)%20AND%20time%20%3E%3D%20now()%20-%203h%20GROUP%20BY%20time(15s)%20fill(null)&epoch=ms'
-  // )
-
-  return (
-    <RealTime>
-      <ListTemp>
-        <ListComponents>
-          <CompoLeft>
-            <h1>Max</h1>
-          </CompoLeft>
-          <CompoRight>
-            <Value>{maxTemp}</Value>
-          </CompoRight>
-        </ListComponents>
-        <ListComponents>
-          <CompoLeft>
-            <h1>Min</h1>
-          </CompoLeft>
-          <CompoRight>
-            <Value>{minTemp}</Value>
-          </CompoRight>
-        </ListComponents>
-        <ListComponents>
-          <CompoLeft>
-            <h1>Average</h1>
-          </CompoLeft>
-          <CompoRight>{/* <DisplayAvgTemporature /> */}</CompoRight>
-        </ListComponents>
-      </ListTemp>
-      <BoxRealTime>
-        <RTDetails>
-          <Top>
-            <h2>NOW</h2>
-          </Top>
-          <Bottom>
-            <div>{temp}</div>
-          </Bottom>
-        </RTDetails>
-      </BoxRealTime>
-    </RealTime>
-  )
->>>>>>> 577cd62abff980cde23b738dee2f7498330bf416
 }
 
 export default List
--- react-app/src/components/Ranking/index.js
+++ react-app/src/components/Ranking/index.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TempValueContextWrapper } from '../../contexts/TempValueContext'
 import TableCard from '../../layout/Table'
 import { RankingComponents, RankingDetails } from './RankingElements'
 
 const Ranking = () => {
+  const {temp1,temp2,maxTemp,minTemp,tempAvg}= useContext(TempValueContextWrapper)
   return (
     <RankingComponents>
       <h1>RANKING</h1>
       <RankingDetails>
         <TableCard No="NO." Location="Location" Temperature="Temperature"></TableCard>
+
+        {temp1 > temp2 ?
+          <>
+            <TableCard No="1" Location="Artisan" Temperature={temp1}/>
+            <TableCard No="2" Location="My Home" Temperature={temp2}/>
+          </>
+          :
+          <>
+            <TableCard No="1" Location="My Home" Temperature={temp2}/>
+            <TableCard No="2" Location="Artisan" Temperature={temp1}/>
+          </>
+        }
       </RankingDetails>
     </RankingComponents>
   )

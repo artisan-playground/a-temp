4eb39d9b4f610b07b01a4f51b764eab813113433
Supatat Singkomfu <Supatat Singkomfu>
2020-12-13 16:05:31 +0700

    Edit Ranking

--- react-app/src/layout/Table.js
+++ react-app/src/layout/Table.js
@@ -2,59 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Table = styled.div`
-    display: flex;
-    font-weight: 900;
-    &:first-child {
-      .cell-table {
-        padding: 14px 0px;
-        text-align: center;
-        flex: 1;
-        &.menu-table {
-          border-top: 0px;
-        }
-      }
-      .date,
-      .price {
-        flex: 1;
-      }
-      .farm {
-        flex: 1;
-        i {
-          font-size: 15px;
-        }
-      }
-      .name {
-        flex: 1px;
-      }
-    }
-    &:last-child {
-      .cell-table {
-        padding: 14px 0px;
-        text-align: center;
-        flex: 1;
-        &.menu-table {
-          border-top: 0px;
-        }
-      }
-      .date,
-      .price {
-        flex: 1;
-      }
-      .name {
-        flex: 1px;
-        text-align: left;
-      }
-      .farm {
-        flex: 1;
-        i {
-          font-size: 15px;
-        }
-      }
-    }
+  display: flex;
+  font-weight: 900;
+  font-size: 20px;
+  margin-top: 10px;
+  &:first-child {
     .cell-table {
       padding: 14px 0px;
       text-align: center;
       flex: 1;
+      &.menu-table {
+        border-top: 0px;
+      }
     }
     .date,
     .price {
@@ -68,28 +27,64 @@ const Table = styled.div`
     }
     .name {
       flex: 1px;
-      text-align: left;
-      p{
-          margin-left: 15%;
+    }
+  }
+  &:last-child {
+    .cell-table {
+      padding: 14px 0px;
+      text-align: center;
+      flex: 1;
+      &.menu-table {
+        border-top: 0px;
       }
     }
+    .date,
+    .price {
+      flex: 1;
+    }
+    .name {
+      flex: 1px;
+      text-align: left;
+    }
+    .farm {
+      flex: 1;
+      i {
+        font-size: 15px;
+      }
+    }
+  }
+  .cell-table {
+    padding: 14px 0px;
+    text-align: center;
+    flex: 1;
+  }
+  .date,
+  .price {
+    flex: 1;
+  }
+  .farm {
+    flex: 1;
+    i {
+      font-size: 15px;
+    }
+  }
+  .name {
+    flex: 1px;
+    text-align: left;
+    p {
+      margin-left: 15%;
+    }
+  }
 `
 
-
-const TableCard = ({No, Location, Temperature}) => {
-    return (
-        <Table>
-            <div class="cell-table date">
-                {No}
-            </div>
-            <div class="cell-table name-header">
-                {Location}
-            </div>
-            <div class="cell-table farm">
-                {Temperature}
-            </div>
-        </Table>
-    )
+const TableCard = ({ No, Location, Temperature }) => {
+  return (
+    <Table>
+      <div class="cell-table date">{No}</div>
+      <div class="cell-table name-header">{Location}</div>
+      <div class="cell-table farm">{Temperature}</div>
+    </Table>
+  )
 }
 
 export default TableCard

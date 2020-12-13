c9a7def5758df10c4a3bed4d0f59df4ab742b4d1
Your Name <you@example.com>
2020-12-14 02:50:47 +0700

    Edit ListElements.js in List components

--- react-app/src/components/List/ListElements.js
+++ react-app/src/components/List/ListElements.js
@@ -10,46 +10,6 @@ export const ListTemp = styled.div`
     flex-direction: column;
 `
 
-export const ListComponents = styled.div`
-    width: 300px;
-    display: flex;
-    flex-direction: row;
-    margin: 15px 0 15px 15px;
-    justify-content: right;
-`
-
-export const CompoLeft = styled.div`
-    background-color: #66CDAA;
-    width: 40%;
-    height: 100px;
-    border-radius: 30px 0 0 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    font-weight: bold;
-    font-size: 25px;
-`
-
-export const CompoRight = styled.div`
-    background-color: #fff;
-    width: 50%;
-    height: 100px;
-    border-radius: 0 30px 30px 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    h1 {
-        font-size: 30px;
-        margin: 5px;
-    font-weight: bold;
-    }
-    p {
-        font-size: 15px;
-    }
-`
-
 export const RealTime = styled.div`
     width: 100%;
     display: flex;

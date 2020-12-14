5bd7b2f5ddc8ffbf142007f676c8ccd6bf369fb7
Supatat Singkomfu <Supatat Singkomfu>
2020-12-14 20:16:08 +0700

    Edit Ranking in components

--- react-app/src/components/Ranking/index.js
+++ react-app/src/components/Ranking/index.js
@@ -1,25 +1,27 @@
 import React, { useContext } from 'react'
+import BgRanking from '../../assets/BgRanking.png'
 import { TempValueContextWrapper } from '../../contexts/TempValueContext'
 import TableCard from '../../layout/Table'
 import { RankingComponents, RankingDetails } from './RankingElements'
 
 const Ranking = () => {
-  const { temp1, temp2} = useContext(TempValueContextWrapper)
+  const { temp1, temp2 } = useContext(TempValueContextWrapper)
   return (
     <RankingComponents>
       <h1>RANKING</h1>
       <RankingDetails>
+        <img src={BgRanking} />
         <TableCard No="NO." Location="Location" Temperature="Temperature"></TableCard>
 
         {temp1 > temp2 ? (
           <>
-            <TableCard No="1" Location="Artisan" Temperature={temp1 +"  °C"} />
-            <TableCard No="2" Location="My Home" Temperature={temp2 +"  °C"} />
+            <TableCard No="1" Location="Artisan" Temperature={temp1 + '  °C'} />
+            <TableCard No="2" Location="My Home" Temperature={temp2 + '  °C'} />
           </>
         ) : (
           <>
-            <TableCard No="1" Location="My Home" Temperature={temp2 +"  °C"} />
-            <TableCard No="2" Location="Artisan" Temperature={temp1 +"  °C"} />
+            <TableCard No="1" Location="My Home" Temperature={temp2 + '  °C'} />
+            <TableCard No="2" Location="Artisan" Temperature={temp1 + '  °C'} />
           </>
         )}
       </RankingDetails>

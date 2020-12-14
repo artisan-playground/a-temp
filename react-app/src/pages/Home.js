b3d74020c8ab5a9ea2d48d8d28daf4a943b10dd1
Supatat Singkomfu <Supatat Singkomfu>
2020-12-14 20:55:39 +0700

    Edit Home

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,34 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
 import Banner from '../assets/banner.png'
+import BgGraph from '../assets/BgGraph.png'
+import BgRanking from '../assets/BgRanking.png'
 import Graph from '../components/Graph'
 import List from '../components/List'
 import Ranking from '../components/Ranking'
 
 const Title = styled.div`
-    padding-top: 5vh;
-    img{
-        width: 100%;
-    }
+  padding-top: 5vh;
+  img {
+    width: 100%;
+  }
 `
 
 const Wrapper = styled.div`
-    padding: 2% 10%;
+  padding: 2% 10%;
+  z-index: 1000;
+  img {
+    position: absolute;
+    z-index: -1;
+  }
 `
 
 const Home = () => {
-    return (
-        <>
-            <Title>
-            <img src={Banner} />
-            </Title>
-            <Wrapper>
-                <List />
-                <Graph />
-                <Ranking />
-            </Wrapper>
-        </>
-    )
+  return (
+    <>
+      <Title>
+        <img src={Banner} />
+      </Title>
+      <Wrapper>
+        <List />
+        <img src={BgGraph} />
+        <Graph />
+        <img src={BgRanking} />
+        <Ranking />
+      </Wrapper>
+    </>
+  )
 }
 
 export default Home

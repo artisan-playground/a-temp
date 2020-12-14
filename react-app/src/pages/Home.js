e62d1588768f666c5603c40cf5f523732b9c6539
Supatat Singkomfu <Supatat Singkomfu>
2020-12-15 05:02:46 +0700

    Edit Home

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ScrollspyNav from 'react-scrollspy-nav'
 import styled from 'styled-components'
 import Banner from '../assets/banner.png'
 import BgGraph from '../assets/BgGraph.png'
 import BgRanking from '../assets/BgRanking.png'
+import Logo from '../assets/logo.png'
 import Graph from '../components/Graph'
 import List from '../components/List'
+import { Bars, Nav, NavLink, NavLogo, NavMenu } from '../components/Navbar/NavBarElements'
 import Ranking from '../components/Ranking'
 
 const Title = styled.div`
@@ -23,21 +26,54 @@ const Wrapper = styled.div`
   }
 `
 
-const Home = () => {
-  return (
-    <>
-      <Title>
-        <img src={Banner} />
-      </Title>
-      <Wrapper>
-        <List />
-        <img src={BgGraph} />
-        <Graph />
-        <img src={BgRanking} />
-        <Ranking />
-      </Wrapper>
-    </>
-  )
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <ScrollspyNav
+          scrollTargetIds={['section_1', 'section_2', 'section_3']}
+          offset={-100}
+          activeNavClass="is-active"
+          scrollDuration="500"
+          headerBackground="true"
+        >
+          <Nav>
+            <NavLogo to="/">
+              <img src={Logo} />
+            </NavLogo>
+            <Bars />
+            <NavMenu>
+              <NavLink>
+                <a href="#section_1">MAP</a>
+              </NavLink>
+              <NavLink>
+                <a href="#section_2">GRAPH</a>
+              </NavLink>
+              <NavLink>
+                <a href="#section_3">RANKING</a>
+              </NavLink>
+            </NavMenu>
+          </Nav>
+        </ScrollspyNav>
+        <Title>
+          <img src={Banner} />
+        </Title>
+        <Wrapper>
+          <div id="section_1">
+            <List />
+          </div>
+          <img src={BgGraph} />
+          <div id="section_2">
+            <Graph />
+          </div>
+          <img src={BgRanking} />
+          <div id="section_3">
+            <Ranking />
+          </div>
+        </Wrapper>
+      </>
+    )
+  }
 }
 
 export default Home

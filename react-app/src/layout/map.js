6c5ad607899d20d4929909a2289e2fe8f76644b1
sahatsawat1 <sahatsawatfriend@gmail.com>
2020-12-15 08:34:02 +0700

    Edit map

--- react-app/src/layout/map.js
+++ react-app/src/layout/map.js
@@ -1,40 +1,40 @@
 import GoogleMapReact from 'google-map-react';
 import React, { Component } from 'react';
-import marker from '../assets/icons8-marker-96.png';
+import Marker from '../assets/icons8-marker-96.png';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-const handleApiLoaded = (map, maps) => {
-  // use map and maps objects
-};
+const AnyReactComponent = ({ Marker }) => <div><img src={Marker} style={{ width: '24px'}} /></div>;
 
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
-      lat: 18.785933,
-      lng: 98.967194
+      lat: 18.787517,
+      lng: 98.974227
     },
-    zoom: 1000
-  };
 
+    zoom: 15
+  };
 
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '47vh', width: '97%' }}>
-
+      <div style={{ height: '47vh', width: '97%'}}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyAqU5DvPyolKD9PLRQyAoQ4J3vBOnyibh8" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
         >
           <AnyReactComponent
             lat={18.785933}
             lng={98.967194}
-            icon={marker}
+            Marker={Marker}
           />
+
+          <AnyReactComponent
+            lat={18.79038685231353}
+            lng={98.9824051139253}
+            Marker={Marker}
+          />
+
         </GoogleMapReact>
       </div>
     );
